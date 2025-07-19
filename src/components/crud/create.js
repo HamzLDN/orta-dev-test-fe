ee490420243d24be1dbb1d91d32d352d9f0f0d63
HamzLDN <hamchenhbf3@gmail.com>
2025-07-19 15:53:21 +0100

    CRUD folder
    We will Create, Read, Update, Delete records

---
+++ src/components/crud/create.js
@@ -0,0 +1,27 @@
+const createShift = async (formData, toggleOverlay, setFormData, data_to_submit) => {
+    try {
+      const res = await fetch('http://localhost:8000/api/shifts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('authToken').replace(/^"|"$/g, '')}`,
+        },
+        body: JSON.stringify(formData),
+      });
+  
+      if (res.ok) {
+        alert('Shift created!');
+        if (typeof toggleOverlay === 'function') toggleOverlay();
+        if (typeof setFormData === 'function') setFormData(data_to_submit);
+      } else {
+        const data = await res.json();
+        alert(data.message);
+      }
+    } catch (error) {
+      console.error('Error creating shift:', error);
+      alert('An unexpected error occurred');
+    }
+  };
+  
+  export default createShift;
+  
---
+++ src/components/crud/delete.js
@@ -0,0 +1,20 @@
+function deleteShift(id) {
+    // It sends a DELETE request to the API endpoint with the shift ID
+    fetch(`http://localhost:8000/api/shifts/${id}`, {
+        method: "DELETE",
+        headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${localStorage.getItem("authToken").replace(/^"|"$/g, '')}`,
+        },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log("Shift deleted:", data);
+    return true
+    })
+    .catch((error) => {
+    console.error("Error deleting shift:", error);
+    });
+}
+
+export default deleteShift;
