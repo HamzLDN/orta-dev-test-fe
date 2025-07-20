36ac42a52e9dce366bd04281894df7a9796af275
HamzLDN <hamchenhbf3@gmail.com>
2025-07-20 17:33:00 +0100

    Update delete.js
    try block incase of any erros we can handle the exceptions accordingly

--- src/components/crud/delete.js
+++ src/components/crud/delete.js
@@ -1,20 +1,26 @@
 async function deleteShift(id) {
-    // It sends a DELETE request to the API endpoint with the shift ID
-    fetch(`http://localhost:8000/api/shifts/${id}`, {
+    try {
+      const response = await fetch(`http://localhost:8000/api/shifts/${id}`, {
         method: "DELETE",
         headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${localStorage.getItem("authToken").replace(/^"|"$/g, '')}`,
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${localStorage.getItem("authToken").replace(/^"|"$/g, '')}`,
         },
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        console.log("Shift deleted:", data);
-    return true
-    })
-    .catch((error) => {
-    console.error("Error deleting shift:", error);
-    });
-}
+      });
+  
+      if (!response.ok) {
+        const errorText = await response.text();
+        console.error("Delete failed:", errorText);
+        return false;
+      }
+  
+      const data = await response.json();
+      console.log("Shift deleted:", data);
+      return true;
+    } catch (error) {
+      console.error("Error deleting shift:", error);
+      return false;
+    }
+  }
 
 export default deleteShift;
